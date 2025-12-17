9d78662d3b7a1ead6cdd68581c3dbdad0a001926
Sanchit Khare <sanchitkhare3007@gmail.com>
2025-12-17 17:59:22 +0530

    Prepare backend for deployment

--- app.py
+++ app.py
@@ -40,6 +40,9 @@ def recommend():
 
     return jsonify(results.to_dict(orient="records"))
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
